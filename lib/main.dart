2b006e8166b46387e3b8a4d54cee20c73eafb95b
taral desai <129868515+taraldesai10@users.noreply.github.com>
2024-10-14 01:06:41 +0530

    1:06

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:stack_note/note_stash.dart';
 
+<<<<<<< Updated upstream
 void main() {
   runApp(const NoteStash());
 }
+=======
+void main(){
+  runApp(const NoteStash());
+}
+
+>>>>>>> Stashed changes
