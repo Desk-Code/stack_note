59a7f354d773d85580881a28f5d9275b00461e05
Harsh Sangani <harshsangani001@gmail.com>
2024-10-14 01:16:57 +0530

    login on work

---
+++ lib/modules/login/controller/login.controller.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: ,
+        leading: IconButton(
+          onPressed: () {
+            Get.back();
+          },
+          icon: const Icon(Icons.arrow_back_rounded),
+        ),
+        title: Text(
+          'appName'.tr,
+          style: GoogleFonts.lato(
+            color: Colors.black,
+            fontSize: 25,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+}
